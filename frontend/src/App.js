faf498ca78eb59e9e316c19fd103151df86aae31
anna_reirl <anna.reil.student@gmail.com>
2022-05-14 17:10:45 +0400

    feat: Create-Shipping-Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import { Link } from 'react-router-dom';
 import { Store } from './Store';
 import SigninScreen from './screens/SigninScreen';
 import CartScreen from './screens/CartScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -74,6 +75,10 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route
+                path="/shipping"
+                element={<ShippingAddressScreen />}
+              ></Route>
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
